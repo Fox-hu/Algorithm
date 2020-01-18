297fe3e61c936896beb2059e9e532f7b721a2d61
Fox.hu <Hujia0523@gmail.com>
2020-01-18 13:09:10 +0800

    添加 递归逆序栈
    添加 递归逆序栈

---
+++ app/src/main/java/com/fox/algorithm/interview/ReverseStack3.kt
@@ -0,0 +1,28 @@
+package com.fox.algorithm.interview
+
+import java.util.*
+
+/**
+ * 仅仅使用递归函数来逆序一个栈
+ * @Author fox
+ * @Date 2020/1/18 12:00
+ */
+
+fun getAndRemoveLastElement(stack: Stack<Int>): Int {
+    val result = stack.pop()
+    return if (stack.empty()) {
+        result
+    } else {
+        val last = getAndRemoveLastElement(stack)
+        stack.push(result)
+        last
+    }
+}
+
+fun reverse(stack: Stack<Int>){
+    if(!stack.empty()){
+        val lastElement = getAndRemoveLastElement(stack)
+        reverse(stack)
+        stack.push(lastElement)
+    }
+}
