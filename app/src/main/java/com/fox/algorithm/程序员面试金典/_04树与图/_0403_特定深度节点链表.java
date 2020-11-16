35399db5a9e618de8b6c12971b8eff4ed670256b
Fox.hu <Hujia0523@gmail.com>
2020-11-16 22:07:40 +0800

    add 0403
    add 0403

---
+++ app/src/main/java/com/fox/algorithm/程序员面试金典/_04树与图/_0403_特定深度节点链表.java
@@ -0,0 +1,56 @@
+package com.fox.algorithm.程序员面试金典._04树与图;
+
+import com.fox.algorithm.程序员面试金典._02链表.ListNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author Fox
+ * @Date 2020/11/16 21:59
+ */
+//链接：https://leetcode-cn.com/problems/list-of-depth-lcci
+//给定一棵二叉树，设计一个算法，创建含有某一深度上所有节点的链表（比如，若一棵树的深度为 D，则会创建出 D 个链表）。返回一个包含所有深度的链表的数组。
+//示例：
+//
+//输入：[1,2,3,4,5,null,7,8]
+//
+//        1
+//       /  \
+//      2    3
+//     / \    \
+//    4   5    7
+//   /
+//  8
+//
+//输出：[[1],[2,3],[4,5,7],[8]]
+class _0403_特定深度节点链表 {
+
+    public ListNode[] listOfDepth(TreeNode tree) {
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(tree);
+
+        List<ListNode> res = new ArrayList<>();
+        ListNode dummy = new ListNode(0);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ListNode curr = dummy;
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = queue.poll();
+                curr.setNext(new ListNode(treeNode.getVal()));
+                if (treeNode.getLeft() != null) {
+                    queue.offer(treeNode.getLeft());
+                }
+                if (treeNode.getRight() != null) {
+                    queue.offer(treeNode.getRight());
+                }
+                curr = curr.getNext();
+            }
+            res.add(dummy.getNext());
+            dummy.setNext(null);
+        }
+
+        return res.toArray(new ListNode[]{});
+    }
+}
