f8858ba51d64aa7b91047622a32178490b96ffd1
Fox.hu <Hujia0523@gmail.com>
2021-02-01 22:42:35 +0800

    动态规划解决最大子序列
    动态规划解决最大子序列

--- app/src/main/java/com/fox/algorithm/数据结构与算法/算法/分治/最大子序列和.kt
+++ app/src/main/java/com/fox/algorithm/数据结构与算法/算法/分治/最大子序列和.kt
@@ -11,30 +11,31 @@ package com.fox.algorithm.数据结构与算法.算法.分治
 //解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。 采用分治方式来试试
 
 
-fun maxSubArray(nums: IntArray): Int {
-    return maxSubArray(nums, 0, nums.size)
+fun maxSubArrayByDivide(nums: IntArray): Int {
+    if (nums.isEmpty()) return 0
+    return maxSubArrayByDivide(nums, 0, nums.size)
 }
 
 //有三种可能 分别是在数组的左侧、数组的右侧以及左右侧各占一部分
 //所以结果就是比较三者的最大值
-fun maxSubArray(nums: IntArray, begin: Int, end: Int): Int {
+fun maxSubArrayByDivide(nums: IntArray, begin: Int, end: Int): Int {
     if (end - begin < 2) return nums[begin]
     val mid = (begin + end) shr 1
 
     //以mid为初始，依次像左边加 获得左侧最大的值
     var leftMax = nums[mid - 1]
-    var leftSum = 0
+    var leftSum = leftMax
     for (i in mid - 2 downTo begin) {
         leftSum += nums[i]
         leftMax = Math.max(leftSum, leftMax)
     }
 
     var rightMax = nums[mid]
-    var rightSum = 0
+    var rightSum = rightMax
     for (i in mid + 1 until end) {
         rightSum += nums[i]
         rightMax = Math.max(rightMax, rightSum)
     }
     //比较三种情况下 那种获得的值最大
-    return Math.max(leftMax + rightMax, Math.max(maxSubArray(nums, begin, mid), maxSubArray(nums, mid, end)))
+    return Math.max(leftMax + rightMax, Math.max(maxSubArrayByDivide(nums, begin, mid), maxSubArrayByDivide(nums, mid, end)))
 }
