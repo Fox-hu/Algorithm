4eaadcd74e69243da0c64adcdf82d597d43b30de
Fox.hu <Hujia0523@gmail.com>
2021-02-03 22:05:43 +0800

    最长公共子序列
    最长公共子序列

--- app/src/main/java/com/fox/algorithm/数据结构与算法/算法/动态规划/最长上升子序列.kt
+++ app/src/main/java/com/fox/algorithm/数据结构与算法/算法/动态规划/最长上升子序列.kt
@@ -4,24 +4,37 @@ package com.fox.algorithm.数据结构与算法.算法.动态规划
  * @Author Fox
  * @Date 2021/2/2 21:10
  */
-//链接：https://leetcode-cn.com/problems/longest-common-subsequence
-//给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
-//一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
-//例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
-//若这两个字符串没有公共子序列，则返回 0。
-//示例 1:                                    示例 2:                                    示例 3:
-//输入：text1 = "abcde", text2 = "ace"        输入：text1 = "abc", text2 = "abc"         输入：text1 = "abc", text2 = "def"
-//输出：3                                     输出：3                                    输出：0
-//解释：最长公共子序列是 "ace"，它的长度为 3。   解释：最长公共子序列是 "abc"，它的长度为 3。 解释：两个字符串没有公共子序列，返回 0。
-//提示:
-//1 <= text1.length <= 1000
-//1 <= text2.length <= 1000
-//输入的字符串只含有小写英文字符。
+/*链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
+给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+子序列是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+示例 1：
+输入：nums = [10,9,2,5,3,7,101,18]
+输出：4
+解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
 
-//dp[i] 是以nums[i]为结尾的最长上升子序列的长度
-//遍历 j ∈ [0, i),当 nums[i] > nums[j]时,nums[i] 可以接在 nums[j] 后面，形成一个比 dp(j) 更长的上升子序列，长度为 dp(j) + 1
-//dp(i) = max { dp(i), dp(j) + 1 }
-//当 nums[i] ≤ nums[j]时nums[i] 不能接在 nums[j] 后面，跳过此次遍历（continue）
+示例 2：
+输入：nums = [0,1,0,3,2,3]
+输出：4
+
+示例 3：
+输入：nums = [7,7,7,7,7,7,7]
+输出：1
+
+提示：
+1 <= nums.length <= 2500
+-104 <= nums[i] <= 104
+ 
+进阶：
+你可以设计时间复杂度为 O(n2) 的解决方案吗？
+你能将算法的时间复杂度降低到 O(n log(n)) 吗?
+*/
+
+/* 动态规划解决
+dp[i] 是以nums[i]为结尾的最长上升子序列的长度
+遍历 j ∈ [0, i),当 nums[i] > nums[j]时,nums[i] 可以接在 nums[j] 后面，形成一个比 dp(j) 更长的上升子序列，长度为 dp(j) + 1
+dp(i) = max { dp(i), dp(j) + 1 }
+当 nums[i] ≤ nums[j]时nums[i] 不能接在 nums[j] 后面，跳过此次遍历（continue）
+*/
 fun lengthOfLIS(nums: IntArray): Int {
     if (nums.isEmpty()) return 0
     val dp = IntArray(nums.size)
---
+++ app/src/main/java/com/fox/algorithm/数据结构与算法/算法/动态规划/最长公共子序列.kt
@@ -0,0 +1,44 @@
+package com.fox.algorithm.数据结构与算法.算法.动态规划
+
+/**
+ * @Author Fox
+ * @Date 2021/2/3 20:55
+ */
+/*链接：https://leetcode-cn.com/problems/longest-common-subsequence
+给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+若这两个字符串没有公共子序列，则返回 0。
+示例 1:                                    示例 2:                                    示例 3:
+输入：text1 = "abcde", text2 = "ace"        输入：text1 = "abc", text2 = "abc"         输入：text1 = "abc", text2 = "def"
+输出：3                                     输出：3                                    输出：0
+解释：最长公共子序列是 "ace"，它的长度为 3。   解释：最长公共子序列是 "abc"，它的长度为 3。 解释：两个字符串没有公共子序列，返回 0。
+提示:
+1 <= text1.length <= 1000
+1 <= text2.length <= 1000
+输入的字符串只含有小写英文字符。*/
+
+/*
+ 采用递归的方式
+ 假设 dp(i, j) 是【nums1 前 i 个元素】与【nums2 前 j 个元素】的最长公共子序列长度
+ dp(i, 0)、dp(0, j) 初始值均为 0,如果 nums1[i – 1] = nums2[j – 1]，那么 dp(i, j) = dp(i – 1, j – 1) + 1
+ 如果 nums1[i – 1] ≠ nums2[j – 1]，那么 dp(i, j) = max { dp(i – 1, j), dp(i, j – 1) }
+*/
+fun lengthLCS(nums1: IntArray, nums2: IntArray): Int {
+    if (nums1.isEmpty() || nums2.isEmpty()) return 0
+    return lengthLCS(nums1, nums1.size, nums2, nums2.size)
+}
+
+fun lengthLCS(nums1: IntArray, i: Int, nums2: IntArray, j: Int): Int {
+    if (i == 0 || j == 0) return 0
+    if (nums1[i - 1] == nums2[j - 1]) {
+        return lengthLCS(nums1, i - 1, nums2, j - 1) + 1
+    } else {
+        return Math.max(lengthLCS(nums1, i, nums2, j - 1),
+                lengthLCS(nums1, i - 1, nums2, j))
+    }
+}
+
+fun main() {
+    println(lengthLCS(intArrayOf(1, 3, 5, 9, 10), intArrayOf(1, 4, 9, 10)))
+}
