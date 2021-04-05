4a045d17526f96171356db4588ad5144ec49f59a
Fox.hu <Hujia0523@gmail.com>
2021-04-05 21:51:39 +0800

    206 21转java
    206 21转java

--- app/src/main/java/com/fox/algorithm/数据结构与算法/leetcode/链表/_21合并两个有序链表.java
+++ app/src/main/java/com/fox/algorithm/数据结构与算法/leetcode/链表/_21合并两个有序链表.java
@@ -4,6 +4,18 @@ package com.fox.algorithm.数据结构与算法.leetcode.链表;
  * @Author Fox
  * @Date 2021/3/23 10:44
  */
+
+/*https://leetcode-cn.com/problems/merge-two-sorted-lists/
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+输入：l1 = [], l2 = [0]
+输出：[0] */
+
 class _21合并两个有序链表 {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
@@ -28,5 +40,19 @@ class _21合并两个有序链表 {
         }
         head.next = l1 == null ? l2 : l1;
         return preHead.next;
+    }
+
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l2.val > l1.val) {
+            l1.next = mergeTwoLists1(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists1(l2.next, l1);
+            return l2;
+        }
     }
 }
