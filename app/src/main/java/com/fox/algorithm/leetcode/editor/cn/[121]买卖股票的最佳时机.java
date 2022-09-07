7ed158212b1b3a9b73b0ec13faf4ce5ad7a7c960
hujia <hujia0523@gmail.com>
2022-09-07 22:34:13 +0800

    add no 121 solution

---
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[121]买卖股票的最佳时机.java
@@ -0,0 +1,59 @@
+package com.fox.algorithm.leetcode.editor.cn;
+/**
+给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+
+ 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+
+ 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+
+
+ 示例 1：
+
+
+输入：[7,1,5,3,6,4]
+输出：5
+解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+
+
+ 示例 2：
+
+
+输入：prices = [7,6,4,3,1]
+输出：0
+解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+
+
+
+
+ 提示：
+
+
+ 1 <= prices.length <= 10⁵
+ 0 <= prices[i] <= 10⁴
+
+
+ Related Topics 数组 动态规划 👍 2548 👎 0
+
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    //思路在于 记录下最低的买入时机
+    //在非最低买入时 计算卖出-最低买入的收益
+    //记录收益最大值
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int min = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < prices[min]) {
+                min = i;
+            } else if(prices[i] - prices[min] > maxProfit){
+                maxProfit = prices[i] - prices[min];
+            }
+        }
+        return maxProfit;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
