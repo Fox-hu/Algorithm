f41885791385f9f068f955507fcb8ba4d6a0f425
fox <hujia0523@gmail.com>
2024-02-18 18:50:00 +0800

    add 49/128/283

---
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[128]最长连续序列.java
@@ -0,0 +1,63 @@
+//给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。 
+//
+// 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [100,4,200,1,3,2]
+//输出：4
+//解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,3,7,2,5,8,4,6,0,1]
+//输出：9
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= nums.length <= 10⁵ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// 
+//
+// Related Topics 并查集 数组 哈希表 👍 1976 👎 0
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        //1.使用set 过滤掉重复数据
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int max = 0;
+        //遍历nums，对于当前num 如果set中没有num-1 那么长度就应该重置为1
+        //如果set中有num+1 那么继续去找num+2\3...n 找到最长长度
+        //更新循环的最大长度 遍历结束时 即可得出最大长度
+        for (Integer num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentSize = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentSize += 1 ;
+                }
+                max = Math.max(max,currentSize);
+            }
+        }
+        return max;
+    }
+}
+        //leetcode submit region end(Prohibit modification and deletion)
---
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[49]字母异位词分组.java
@@ -0,0 +1,60 @@
+//给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。 
+//
+// 字母异位词 是由重新排列源单词的所有字母得到的一个新单词。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+//输出: [["bat"],["nat","tan"],["ate","eat","tea"]] 
+//
+// 示例 2: 
+//
+// 
+//输入: strs = [""]
+//输出: [[""]]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: strs = ["a"]
+//输出: [["a"]] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= strs.length <= 10⁴ 
+// 0 <= strs[i].length <= 100 
+// strs[i] 仅包含小写字母 
+// 
+//
+// Related Topics 数组 哈希表 字符串 排序 👍 1818 👎 0
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key,list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
