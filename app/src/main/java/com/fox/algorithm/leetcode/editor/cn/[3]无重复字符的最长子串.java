0a0671a50913c9a4be5d7e7fa69d3d675dbb71cb
fox <hujia0523@gmail.com>
2024-02-19 16:20:49 +0800

    add 3/11/15

---
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[3]无重复字符的最长子串.java
@@ -0,0 +1,70 @@
+//给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: s = "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 5 * 10⁴ 
+// s 由英文字母、数字、符号和空格组成 
+// 
+//
+// Related Topics 哈希表 字符串 滑动窗口 👍 9975 👎 0
+
+
+import java.util.HashMap;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        //移动窗口
+        //使用map记录char和index的信息
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 1;
+        int left = 0;
+        //当map中没有char时 left不用移动 长度自然增长1
+        //当map中有该char时 说明之前出现过该char 那么将该char的index+1 移出窗口 left也随之移动
+        //更新char和index的记录
+        //循环完成 得出最大长度
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (map.containsKey(ch)) {
+                left = Math.max(left, map.get(ch) + 1);
+            }
+            map.put(ch, i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
