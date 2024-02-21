15c7c51a0f5f958cb67d647d11aed9defd5778e9
fox <hujia0523@gmail.com>
2024-02-21 12:04:31 +0800

    add 160/206/238

---
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,13 @@
+package com.fox.algorithm.leetcode.editor.cn;
+
+//提交时需要将此import注释掉 否则leetcode编译不过
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
---
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[206]反转链表.java
@@ -0,0 +1,77 @@
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+//
+// 
+// 
+// 
+// 
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 5000] 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+//
+// Related Topics 递归 链表 👍 3501 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import com.fox.algorithm.leetcode.editor.cn.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    //使用双指针，第一个指针叫 pre，最初是指向 null 的。
+    //第二个指针 cur 指向 head，然后不断遍历 cur。
+    //每次迭代到 cur，都将 cur 的 next 指向 pre，然后 pre 和 cur 前进一位。
+    //都迭代完了(cur 变成 null 了)，pre 就是最后一个节点了
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur!=null){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
