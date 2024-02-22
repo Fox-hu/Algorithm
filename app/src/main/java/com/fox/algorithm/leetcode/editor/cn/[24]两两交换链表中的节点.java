77290f4b2abfa82deb27402fa4ebdb3155f30849
fox <hujia0523@gmail.com>
2024-02-22 15:50:27 +0800

    add 19/24/138

---
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[24]两两交换链表中的节点.java
@@ -0,0 +1,67 @@
+//给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4]
+//输出：[2,1,4,3]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 100] 内 
+// 0 <= Node.val <= 100 
+// 
+//
+// Related Topics 递归 链表 👍 2150 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import com.fox.algorithm.leetcode.editor.cn.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    //采用递归的方式
+    //返回值：交换完成的子链表
+    //调用单元：设需要交换的两个点为 head 和 next，head 连接后面交换完成的子链表，next 连接 head，完成交换
+    //终止条件：head 为空指针或者 next 为空指针，也就是当前无节点或者只有一个节点，无法进行交换
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
