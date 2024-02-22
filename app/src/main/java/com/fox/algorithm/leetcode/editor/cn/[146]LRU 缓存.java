dc77672c8a51249cd689722336289d920f290991
fox <hujia0523@gmail.com>
2024-02-22 17:36:39 +0800

    add 146/148

---
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[146]LRU 缓存.java
@@ -0,0 +1,156 @@
+//
+// 请你设计并实现一个满足 
+// LRU (最近最少使用) 缓存 约束的数据结构。
+// 
+//
+// 
+// 实现 
+// LRUCache 类：
+// 
+//
+// 
+// 
+// 
+// LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存 
+// int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。 
+// void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组 
+//key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。 
+// 
+// 
+// 
+//
+// 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入
+//["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+//[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+//输出
+//[null, null, null, 1, null, -1, null, -1, 3, 4]
+//
+//解释
+//LRUCache lRUCache = new LRUCache(2);
+//lRUCache.put(1, 1); // 缓存是 {1=1}
+//lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+//lRUCache.get(1);    // 返回 1
+//lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+//lRUCache.get(2);    // 返回 -1 (未找到)
+//lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+//lRUCache.get(1);    // 返回 -1 (未找到)
+//lRUCache.get(3);    // 返回 3
+//lRUCache.get(4);    // 返回 4
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= capacity <= 3000 
+// 0 <= key <= 10000 
+// 0 <= value <= 10⁵ 
+// 最多调用 2 * 10⁵ 次 get 和 put 
+// 
+//
+// Related Topics 设计 哈希表 链表 双向链表 👍 3081 👎 0
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class LRUCache {
+    //hashMap和双链表 key是链表的val value是链表node
+    //要记住挺难 多看几遍吧
+    private final Map<Integer, DLinkNode> cache = new HashMap<>();
+    private final int capacity;
+    private final DLinkNode head;
+    private final DLinkNode tail;
+    private int size;
+
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        head = new DLinkNode();
+        tail = new DLinkNode();
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkNode node = cache.get(key);
+        if(node == null){
+            return -1;
+        }
+        moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        DLinkNode node = cache.get(key);
+        if (node == null) {
+            DLinkNode newNode = new DLinkNode(key, value);
+            cache.put(key, newNode);
+            addToHead(newNode);
+            size+=1;
+            if (size > capacity) {
+                DLinkNode tail = removeTail();
+                cache.remove(tail.key);
+                size-=1;
+            }
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+
+
+    private void addToHead(DLinkNode node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(DLinkNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void moveToHead(DLinkNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private DLinkNode removeTail() {
+        DLinkNode prev = tail.prev;
+        removeNode(prev);
+        return prev;
+    }
+
+    class DLinkNode {
+        int key;
+        int value;
+        DLinkNode prev;
+        DLinkNode next;
+
+        DLinkNode() {}
+
+        DLinkNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[148]排序链表.java
@@ -0,0 +1,93 @@
+//给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。 
+//
+// 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [4,2,1,3]
+//输出：[1,2,3,4]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [-1,5,3,4,0]
+//输出：[-1,0,3,4,5]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目在范围 [0, 5 * 10⁴] 内 
+// -10⁵ <= Node.val <= 10⁵ 
+// 
+//
+// 
+//
+// 进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？ 
+//
+// Related Topics 链表 双指针 分治 排序 归并排序 👍 2241 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import com.fox.algorithm.leetcode.editor.cn.ListNode;
+
+import java.util.Arrays;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    //偷懒解法 将链表转为数组 排序后重新构造链表
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode cur = head;
+        int length = getListNodeSize(cur);
+        int[] arrays = new int[length];
+        for (int i = 0; i < length; i++) {
+            arrays[i] = cur.val;
+            cur = cur.next;
+        }
+        Arrays.sort(arrays);
+        cur = head;
+        for (int i = 0; i < length; i++) {
+            cur.val = arrays[i];
+            cur = cur.next;
+        }
+        return head;
+    }
+
+    public int getListNodeSize(ListNode cur) {
+        int length = 0;
+        ListNode head = cur;
+        while (head != null) {
+            head = head.next;
+            length++;
+        }
+        return length;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
