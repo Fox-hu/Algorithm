7fc15a23e1f3a2e8463ec2f6aae81d2d87e97dc9
fox <hujia0523@gmail.com>
2024-03-04 16:47:31 +0800

    add 139/279/322

---
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[139]单词拆分.java
@@ -0,0 +1,73 @@
+//给你一个字符串 s 和一个字符串列表 wordDict 作为字典。如果可以利用字典中出现的一个或多个单词拼接出 s 则返回 true。 
+//
+// 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入: s = "leetcode", wordDict = ["leet", "code"]
+//输出: true
+//解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入: s = "applepenapple", wordDict = ["apple", "pen"]
+//输出: true
+//解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+//     注意，你可以重复使用字典中的单词。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+//输出: false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 300 
+// 1 <= wordDict.length <= 1000 
+// 1 <= wordDict[i].length <= 20 
+// s 和 wordDict[i] 仅由小写英文字母组成 
+// wordDict 中的所有字符串 互不相同 
+// 
+//
+// Related Topics 字典树 记忆化搜索 数组 哈希表 字符串 动态规划 👍 2427 👎 0
+
+
+import java.util.Arrays;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    //动态规划 dp[i]代表s的第i位能否使用字典拼接
+    //核心思路在于 如果dp[i] = true，
+    //那么对应于正在遍历的word，dp[i+word.length()] = true
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int length = s.length();
+        boolean[] dp = new boolean[length + 1];
+        Arrays.fill(dp, false);
+        dp[0] = true;
+        for (int i = 0; i < length; i++) {
+            //如果dp[i] == false 那么没有继续下去的必要 所以dp[0] == true
+            if (!dp[i]) {
+                continue;
+            }
+            for (String word : wordDict) {
+                if (word.length() + i <= s.length() && s.startsWith(word, i)) {
+                    dp[i + word.length()] = true;
+                }
+            }
+        }
+        return dp[length];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
