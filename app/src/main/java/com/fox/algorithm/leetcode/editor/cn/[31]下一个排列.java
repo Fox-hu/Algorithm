7f0830879e84cd255b846ea93231f02b6da504b4
hujia <hujia0523@gmail.com>
2024-03-07 23:18:20 +0800

    add comment

--- app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[31]下一个排列.java
+++ app/src/main/java/com/fox/algorithm/leetcode/editor/cn/[31]下一个排列.java
@@ -54,6 +54,11 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    //从后向前 查找第一个 相邻升序 的元素对 (i,j)，满足 A[i] < A[j]。此时 [j,end) 必然是降序
+    //在 [j,end) 从后向前 查找第一个满足 A[i] < A[k] 的 k。A[i]、A[k] 分别就是上文所说的「小数」、「大数」
+    //将 A[i] 与 A[k] 交换
+    //可以断定这时 [j,end) 必然是降序，逆置 [j,end)，使其升序
+    //如果在步骤 1 找不到符合的相邻元素对，说明当前 [begin,end) 为一个降序顺序，则直接跳到步骤 4
     public void nextPermutation(int[] nums) {
         if (nums.length == 1) {
             return;
@@ -86,7 +91,7 @@ class Solution {
         reverse(nums,pos+1);
     }
 
-    //从pos位置处逆序一个数组
+    //从pos位置处反转一个数组
     private void reverse(int[] nums, int pos) {
         for (int i = pos, j = nums.length - 1; i < j; i++, j--) {
             swap(nums, i, j);
